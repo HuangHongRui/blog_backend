12339e0bf82191951d221fc7fbc7e139117b3b49
Leo <464362353@qq.com>
2018-09-02 21:26:54 +0800

    et: ADD: 新增API: 运行时长

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,9 @@
-var express = require('express');
-var router = express.Router();
-var redis = require('redis');
-var db = redis.createClient();
-var app = express();
+let express = require('express');
+let router = express.Router();
+let redis = require('redis');
+let db = redis.createClient();
+let app = express();
+
 /*
  |--------------------------------------------------------------------------
  | 在线人数
@@ -32,5 +33,21 @@ router.get('/online', function (req, res, next) {
     online: `${req.online.length}`
   });
 });
+
+/*
+ |--------------------------------------------------------------------------
+ | 运行历史时间
+ |--------------------------------------------------------------------------
+ */
+router.get('/runtime', (req, res, next) => {
+  res.status(200);
+  res.type('application/json');
+  res.json({
+    state: 1,
+    message: '运行历史时间',
+    unit: '毫秒',
+    runtime: Date.parse(new Date()) - Date.parse(new Date('2018-05-01T09:06:07'))
+  })
+})
 
 module.exports = router;
