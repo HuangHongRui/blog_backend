154f67608da6673aa7f2dd2f24f65b9338d6f94f
Leo.Huang <464362353@qq.com>
2019-01-15 15:13:37 +0800

    [A] 工具方法文件 _ 生成验证码.

---
+++ util/methods.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+  /**
+   *  功能: 生成验证码
+   *  參數：num 生成验证码的长度
+   */
+  generateCode: function (num) {
+    var allCode = "azxcvbnmsdfghjklqwertyuiopZXCVBNMASDFGHJKLQWERTYUIOP0123456789";
+    var resultCode = "";
+    for (let i = 0; i < num; i++) {
+      let index = Math.floor(Math.random() * 62);
+      resultCode += allCode.charAt(index);
+    }
+    return resultCode;
+  }
+
+};
