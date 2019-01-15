3e203ad314c6df432284b17efd762427ea7b019d
Leo.Huang <464362353@qq.com>
2019-01-15 14:56:04 +0800

    [A] 新增MySQL数据库

---
+++ database/dbMysqlMethods.js
@@ -0,0 +1,25 @@
+// CRUD SQL语句
+var user = {
+  INSERT:'INSERT INTO user(id, nickname, email, password) VALUES(0,?,?,?)',
+  update:'update user set nickname=?, email=?, password=? where id=?',
+  delete: 'delete from user where id=?',
+  queryById: 'select * from user where id=?',
+  queryAll: 'select * from user',
+
+  // createTable: 'create table user(\n' +
+  //   '\tid int unsigned not null auto_increment primary key,\n' +
+  //   '\tnickname char(10) not null,\n' +
+  //   '\temail char(50) not null,\n' +
+  //   '\tpassword char(50) not null\n' +
+  //   ');;',
+  // deleteTable: 'DROP TABLE user',
+  // login: 'SELECT * FROM user WHERE nickname=? or email=?',
+
+  register:'INSERT INTO user(id, nickname, email, password) VALUES(0,?,?,?)',
+  login: 'SELECT * FROM user WHERE (nickname=? OR email=?) And password=?',
+  verifyEmail: 'SELECT * FROM user WHERE email=?'
+
+
+};
+
+module.exports = user;
